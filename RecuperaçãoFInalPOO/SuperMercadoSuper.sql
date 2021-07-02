9307c7a0187e3e0ea4d414d1fc2e061b560f38ef
YoruAlptraum <yorualptraum@gmail.com>
2021-07-02 15:34:15 -0300

    Update SuperMercadoSuper.sql

--- RecuperaçãoFInalPOO/SuperMercadoSuper.sql
+++ RecuperaçãoFInalPOO/SuperMercadoSuper.sql
@@ -2,6 +2,7 @@
 dados de produtos (codigo, nome, descrição, preço, porcentagem de desconto) e 
 cliente (cpf e nome).
 */;
+create database sueprmercadosuper;
 use supermercadosuper;
 create table clientes (cpf char(11) primary key not null, nome varchar(255));
 create table produtos (codigo char(13) primary key not null, nome varchar(255) unique,descriçao varchar(255),preço decimal(65,2),desconto decimal(65,2));
@@ -12,11 +13,7 @@ values
 ('0123456123456','pão da casa','saboroso e crocante',1.50,10),
 ('0123456123457','sopa em lata SoSopa','sopa SoSopa diversos sabores',5.25,30);
 
-insert into clientes values('12345678954','Godofredo');
-insert into clientes values('12345678910','Eugenio');
-
-
-select * from produtos;
-select * from clientes;
-
-alter table produtos modify codigo char(13)
+insert into clientes 
+values
+('12345678954','Godofredo'),
+('12345678910','Eugenio');
